b42c40735f7b354eeb99b8cc7e47261eb962366e
isaacleder <isaacleder1002@gmail.com>
2023-02-08 15:45:25 -0300

    ok

---
+++ aula-02-02/exercicio-3.php
@@ -0,0 +1,11 @@
+<?php
+
+class cliente {
+    public $nome;
+    public $email;
+    public $telefone;
+}
+
+public function exibir(){
+    echo "come: " . $this->nome . " email: " . this->email . " telefone: " . this->telefone;
+}
